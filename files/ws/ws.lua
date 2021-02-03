2c55a6cd9c3c74ab194601bb99fee792d1c23758
Soler91 <boskozarich@gmail.com>
2021-02-03 02:52:55 -0400

    desync bandaid

--- files/ws/ws.lua
+++ files/ws/ws.lua
@@ -44,39 +44,41 @@ _ws_main = function()
         return
     end
 
-    local happy, msg = main_socket:poll()
-    
-    if (not happy and count % 1200 == 0) then
-        main_socket = nil
-        reconnect = true
-        increase_count()
-        return
-    end
-    local decoded, data = pcall(json.decode, msg)
-    if (decoded) then
-        if (data.event == "CustomModEvent") then
-            local evt = customEvents[data.payload.name]
-            if (evt ~= nil) then
-                local e, res = pcall(evt, data.payload)
-                --[[if (e) then
-                    print(e)
-                end]]
-            end
-        else
-            --GamePrint(data.event)
-            local evt = wsEvents[data.event]
-            if (evt ~= nil) then
-                local e, res = pcall(evt, data.payload)
-                --[[if (e) then
-                    print(e)
-                end]]
+    for i=1, 25 do -- bandaid for desync
+        local happy, msg = main_socket:poll()
+        
+        if (not happy and count % 1200 == 0) then
+            main_socket = nil
+            reconnect = true
+            increase_count()
+            return
+        end
+        local decoded, data = pcall(json.decode, msg)
+        if (decoded) then
+            if (data.event == "CustomModEvent") then
+                local evt = customEvents[data.payload.name]
+                if (evt ~= nil) then
+                    local e, res = pcall(evt, data.payload)
+                    --[[if (e) then
+                        print(e)
+                    end]]
+                end
+            else
+                --GamePrint(data.event)
+                local evt = wsEvents[data.event]
+                if (evt ~= nil) then
+                    local e, res = pcall(evt, data.payload)
+                    --[[if (e) then
+                        print(e)
+                    end]]
+                end
             end
         end
+        if (count % 60 == 0) then
+            SendWsEvent({event="ping", payload = {}})
+        end
+        increase_count()
     end
-    if (count % 60 == 0) then
-        SendWsEvent({event="ping", payload = {}})
-    end
-    increase_count()
 end
 
 async_loop(function()
