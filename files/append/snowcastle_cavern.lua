757ee99639572156c09cb4b17a0e7bfff57da25a
Soler91 <boskozarich@gmail.com>
2021-02-06 15:49:38 -0400

    add missing check

--- files/append/snowcastle_cavern.lua
+++ files/append/snowcastle_cavern.lua
@@ -1,10 +1,12 @@
 local _spawn_hourglass_blood = spawn_hourglass_blood
 
 function spawn_hourglass_blood(x, y)
-    EntityLoad( "mods/noita-together/files/entities/hourglass_ambrosia.xml", x, y+2 )
-    EntityLoad( "mods/noita-together/files/entities/hourglass_berserk.xml", x, y )
-    EntityLoad( "mods/noita-together/files/entities/hourglass_charm.xml", x, y )
-    EntityLoad( "mods/noita-together/files/entities/hourglass_confusion.xml", x, y )
-    EntityLoad( "mods/noita-together/files/entities/hourglass_speed.xml", x, y )
+    if (GameHasFlagRun("NT_GAMEMODE_CO_OP")) then
+        EntityLoad( "mods/noita-together/files/entities/hourglass_ambrosia.xml", x, y+2 )
+        EntityLoad( "mods/noita-together/files/entities/hourglass_berserk.xml", x, y )
+        EntityLoad( "mods/noita-together/files/entities/hourglass_charm.xml", x, y )
+        EntityLoad( "mods/noita-together/files/entities/hourglass_confusion.xml", x, y )
+        EntityLoad( "mods/noita-together/files/entities/hourglass_speed.xml", x, y )
+    end
     _spawn_hourglass_blood(x, y)
 end
