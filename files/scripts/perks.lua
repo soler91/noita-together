a2114174947a7ebd649551309808347d745acdf4
Soler91 <boskozarich@gmail.com>
2021-02-07 01:13:06 -0400

    move team perk list

---
+++ files/scripts/perks.lua
@@ -0,0 +1,13 @@
+local list = {
+    EXTRA_MONEY=true,
+    STRONG_KICK=true,
+    GLOBAL_GORE=true,
+    EXTRA_HP=true,
+    HEARTS_MORE_EXTRA_HP=true,
+    ATTRACT_ITEMS=true,
+    EXTRA_SHOP_ITEM=true,
+    GENOME_MORE_HATRED=true,
+    GENOME_MORE_LOVE=true,
+    PEACE_WITH_GODS=true
+}
+return list
