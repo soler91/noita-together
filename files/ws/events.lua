62b0b4d986dc47b8ac74f40176ed41d193f1b975
Soler91 <boskozarich@gmail.com>
2021-04-06 17:39:36 -0400

    bump version

--- files/ws/events.lua
+++ files/ws/events.lua
@@ -149,7 +149,7 @@ wsEvents = {
     RequestGameInfo = function(data)
         local seed = StatsGetValue("world_seed")
         local mods = ModGetActiveModIDs()
-        SendWsEvent({event="GameInfo", payload={seed=seed, mods=mods, version="v0.9.60", beta=GameIsBetaBuild()}})
+        SendWsEvent({event="GameInfo", payload={seed=seed, mods=mods, version="v0.9.61", beta=GameIsBetaBuild()}})
         SendWsEvent({event="RequestPlayerList", payload={}})
         PopulateSpellList()
     end,
