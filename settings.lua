44791b765f59f0160e4a89258973d9daae48d610
Soler91 <boskozarich@gmail.com>
2021-04-06 23:41:49 -0400

    toggleable hints setting

---
+++ settings.lua
@@ -0,0 +1,26 @@
+dofile("data/scripts/lib/mod_settings.lua")
+
+local mod_id = "noita-together"
+mod_settings_version = 1
+mod_settings = 
+{
+	{
+		id = "NT_HINTS",
+		ui_name = "Show hints",
+		value_default = true,
+        scope=MOD_SETTING_SCOPE_RUNTIME
+	},
+}
+
+function ModSettingsUpdate( init_scope )
+	local old_version = mod_settings_get_version( mod_id )
+	mod_settings_update( mod_id, mod_settings, init_scope )
+end
+
+function ModSettingsGuiCount()
+	return mod_settings_gui_count( mod_id, mod_settings )
+end
+
+function ModSettingsGui( gui, in_main_menu )
+	mod_settings_gui( mod_id, mod_settings, gui, in_main_menu )
+end
