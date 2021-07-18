4ee443232c61d5a0d43aa57726af4ecc747165c9
clragon <kenta3ds.habbo@gmail.com>
2021-07-18 18:00:43 +0200

    fix indent

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,167 +1,172 @@
 <template>
-  <div class="content">
-    <div class="login-ui">
-      <div class="intro">
-        <img v-bind:src="'./icon/icon.png'" />
-      </div>
-      <div class="login-button" v-if="savedUser">
-        <div class="twitch-login" @click="ContinueSavedUser">
-          <div class="twitch-logo">
-            <i class="fab fa-twitch"></i>
-          </div>
-          <span class="twitch-login-text">
-            Continue as {{ savedUserName }}
-          </span>
+    <div class="content">
+        <div class="login-ui">
+            <div class="intro">
+                <img v-bind:src="'./icon/icon.png'" />
+            </div>
+            <div class="login-button" v-if="savedUser">
+                <div class="twitch-login" @click="ContinueSavedUser">
+                    <div class="twitch-logo">
+                        <i class="fab fa-twitch"></i>
+                    </div>
+                    <span class="twitch-login-text">
+                        Continue as {{ savedUserName }}
+                    </span>
+                </div>
+                <div class="delete-saved-user" @click="DeleteSavedUser">
+                    <i class="fas fa-sign-out-alt"></i>
+                </div>
+            </div>
+            <div class="login-button">
+                <div class="twitch-login" @click="OpenLoginPage">
+                    <div class="twitch-logo">
+                        <i
+                            class="fas fa-spinner fa-spin fa-pulse"
+                            v-if="clicked"
+                        ></i>
+                        <i class="fab fa-twitch" v-else></i>
+                    </div>
+                    <span class="twitch-login-text">
+                        Login with Twitch.tv
+                    </span>
+                </div>
+            </div>
+            <div class="remember-login">
+                <input
+                    type="checkbox"
+                    id="remember-user"
+                    name="remember-user"
+                    v-model="reeemember"
+                />
+                <label for="remember-user">Remember me</label>
+            </div>
         </div>
-        <div class="delete-saved-user" @click="DeleteSavedUser">
-          <i class="fas fa-sign-out-alt"></i>
-        </div>
-      </div>
-      <div class="login-button">
-        <div class="twitch-login" @click="OpenLoginPage">
-          <div class="twitch-logo">
-            <i class="fas fa-spinner fa-spin fa-pulse" v-if="clicked"></i>
-            <i class="fab fa-twitch" v-else></i>
-          </div>
-          <span class="twitch-login-text"> Login with Twitch.tv </span>
-        </div>
-      </div>
-      <div class="remember-login">
-        <input
-          type="checkbox"
-          id="remember-user"
-          name="remember-user"
-          v-model="reeemember"
-        />
-        <label for="remember-user">Remember me</label>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
 import { shell, ipcRenderer } from "electron";
 export default {
-  data() {
-    return {
-      loginUrl: "https://nt.unicast.link:42069/auth/login",
-      reeemember: false,
-      clicked: false,
-    };
-  },
-  beforeCreate() {
-    const unsub = this.$store.subscribe((mutation, state) => {
-      if (mutation.type == "setUser" && state.user.id > 0) {
-        unsub();
-        this.clicked = false;
-        this.$router.replace({ path: "/lobby" });
-        this.$store.commit("setLoading", false);
-      }
-    });
-  },
-  computed: {
-    savedUser() {
-      return this.$store.state.savedUser;
+    data() {
+        return {
+            loginUrl: "https://nt.unicast.link:42069/auth/login",
+            reeemember: false,
+            clicked: false,
+        };
     },
-    savedUserName() {
-      return this.$store.state.savedUserName;
+    beforeCreate() {
+        const unsub = this.$store.subscribe((mutation, state) => {
+            if (mutation.type == "setUser" && state.user.id > 0) {
+                unsub();
+                this.clicked = false;
+                this.$router.replace({ path: "/lobby" });
+                this.$store.commit("setLoading", false);
+            }
+        });
     },
-  },
-  methods: {
-    OpenLoginPage() {
-      this.clicked = true;
-      shell.openExternal(this.loginUrl);
+    computed: {
+        savedUser() {
+            return this.$store.state.savedUser;
+        },
+        savedUserName() {
+            return this.$store.state.savedUserName;
+        },
     },
-    ContinueSavedUser() {
-      this.$store.dispatch("continueSavedUser");
+    methods: {
+        OpenLoginPage() {
+            this.clicked = true;
+            shell.openExternal(this.loginUrl);
+        },
+        ContinueSavedUser() {
+            this.$store.dispatch("continueSavedUser");
+        },
+        DeleteSavedUser() {
+            this.$store.dispatch("deleteSavedUser");
+        },
     },
-    DeleteSavedUser() {
-      this.$store.dispatch("deleteSavedUser");
+    watch: {
+        reeemember(oldVal, newVal) {
+            ipcRenderer.send("remember_user", !newVal);
+        },
     },
-  },
-  watch: {
-    reeemember(oldVal, newVal) {
-      ipcRenderer.send("remember_user", !newVal);
-    },
-  },
 };
 </script>
 
 <style>
 .login-ui {
-  margin: auto;
+    margin: auto;
 }
 
 .login-ui i {
-  font-size: 1.5rem;
+    font-size: 1.5rem;
 }
 
 .login-button {
-  display: flex;
-  margin-top: auto;
-  align-self: center;
-  cursor: pointer;
-  margin: 1em;
+    display: flex;
+    margin-top: auto;
+    align-self: center;
+    cursor: pointer;
+    margin: 1em;
 }
 
 .intro {
-  text-align: center;
-  margin-bottom: 2rem;
+    text-align: center;
+    margin-bottom: 2rem;
 }
 
 .intro > img {
-  height: auto;
-  max-width: 10rem;
+    height: auto;
+    max-width: 10rem;
 }
 
 .twitch-login {
-  display: flex;
-  min-width: 300px;
-  background-color: #6441a5;
-  margin-top: auto;
-  align-self: center;
-  cursor: pointer;
+    display: flex;
+    min-width: 300px;
+    background-color: #6441a5;
+    margin-top: auto;
+    align-self: center;
+    cursor: pointer;
 }
 
 .twitch-login:hover {
-  background-color: #503484;
+    background-color: #503484;
 }
 
 .twitch-logo {
-  padding: 1rem;
-  background-color: #503484;
+    padding: 1rem;
+    background-color: #503484;
 }
 
 .twitch-login-text {
-  margin: auto;
-  align-self: center;
+    margin: auto;
+    align-self: center;
 }
 
 .saved-user {
-  display: flex;
+    display: flex;
 }
 
 .delete-saved-user {
-  padding: 1rem;
-  background-color: #843434;
+    padding: 1rem;
+    background-color: #843434;
 }
 
 .delete-saved-user:hover {
-  padding: 1rem;
-  background-color: #9e3434;
+    padding: 1rem;
+    background-color: #9e3434;
 }
 
 .remember-login {
-  text-align: center;
+    text-align: center;
 }
 
 .remember-login > label {
-  margin: 0.5rem;
+    margin: 0.5rem;
 }
 
 input[type="checkbox"] {
-  vertical-align: middle;
-  position: relative;
-  bottom: 0.1rem;
+    vertical-align: middle;
+    position: relative;
+    bottom: 0.1rem;
 }
 </style>
