a5b772e2547243bd8ecc4ccc08ac0c9e25b9f135
stefnotch <brandmairstefan@gmail.com>
2022-07-27 14:13:33 +0200

    Try to async load sqlite

--- electron/main/database/index.ts
+++ electron/main/database/index.ts
@@ -1,9 +1,7 @@
-import { Kysely, Migrator } from "kysely";
+import type { Kysely } from "kysely";
 import { join } from "path";
 import { app } from "electron";
-import { SqliteDialect } from "./sqlite/sqlite-dialect";
 import * as migration0001 from "./migrations/migration0001";
-import sqlite3 from "sqlite3";
 
 const filePath = join(app.getPath("userData"), "/gamePath.sqlite");
 
@@ -26,53 +24,46 @@ export interface NoitaDatabase {
   bank_item: BankItemTable;
 }
 
-export const dbPromise = new Promise<Kysely<NoitaDatabase>>(
-  (resolve, reject) => {
-    const sqliteDb = new sqlite3.Database(filePath);
-    sqliteDb.configure("busyTimeout", 1000);
-    const noitaDb = new Kysely<NoitaDatabase>({
-      dialect: new SqliteDialect({
-        database: sqliteDb,
-      }),
-    });
+export const dbPromise: Promise<Kysely<NoitaDatabase>> = (async () => {
+  const sqlite3 = await import("sqlite3");
+  const kysely = await import("kysely");
+  const sqliteDialect = await import("./sqlite/sqlite-dialect");
 
-    const migrator = new Migrator({
-      provider: {
-        async getMigrations() {
-          return {
-            migration0001: migration0001,
-          };
-        },
-      },
-      db: noitaDb,
-    });
+  const sqliteDb = new sqlite3.Database(filePath);
+  sqliteDb.configure("busyTimeout", 1000);
+  const noitaDb = new kysely.Kysely<NoitaDatabase>({
+    dialect: new sqliteDialect.SqliteDialect({
+      database: sqliteDb,
+    }),
+  });
 
-    migrator.migrateToLatest().then(
-      (migrationResults) => {
-        const { error, results } = migrationResults;
-        if (error) {
-          reject(error);
-        } else {
-          results?.forEach((it) => {
-            if (it.status === "Success") {
-              console.log(
-                `migration "${it.migrationName}" was executed successfully`
-              );
-            } else if (it.status === "Error") {
-              console.error(
-                `failed to execute migration "${it.migrationName}"`
-              );
-            }
-          });
-          resolve(noitaDb);
-        }
+  const migrator = new kysely.Migrator({
+    provider: {
+      async getMigrations() {
+        return {
+          migration0001: migration0001,
+        };
       },
-      (err) => {
-        reject(err);
+    },
+    db: noitaDb,
+  });
+
+  const { error, results } = await migrator.migrateToLatest();
+  if (error) {
+    throw error;
+  } else {
+    results?.forEach((it) => {
+      if (it.status === "Success") {
+        console.log(
+          `migration "${it.migrationName}" was executed successfully`
+        );
+      } else if (it.status === "Error") {
+        console.error(`failed to execute migration "${it.migrationName}"`);
       }
-    );
+    });
+    return noitaDb;
   }
-);
+})();
 
 // TODO: Put game path in storage
 // TODO: Put world seed, all flags, onDeathKick and gold amount into games storage
